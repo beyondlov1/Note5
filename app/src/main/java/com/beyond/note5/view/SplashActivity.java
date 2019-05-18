c5dce3d056026db7a324bbe40e0961165b841294
beyondlov1 <806784568@qq.com>
2019-05-18 23:54:05 +0800

    增加splash， 和转场动画（不过有一堆无用代码没有删）

---
+++ app/src/main/java/com/beyond/note5/view/SplashActivity.java
@@ -0,0 +1,82 @@
+package com.beyond.note5.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.beyond.note5.R;
+
+public class SplashActivity extends Activity {
+
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+//        Transition transition = TransitionInflater.from(this).inflateTransition(android.R.transition.fade);
+//        getWindow().setExitTransition(transition);
+
+        setContentView(R.layout.activity_splash);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.white));
+
+        initView();
+
+
+    }
+
+    private void initView() {
+        ImageView icon = findViewById(R.id.app_icon);
+        icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_ic_note5_test,null));
+
+        RotateAnimation animation = new RotateAnimation(0,0,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        animation.setDuration(500);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startMainActivity();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        icon.setAnimation(animation);
+
+        animation.start();
+    }
+
+    private void startMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+//        startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        startActivity(intent);
+        finish();
+
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                finish();
+//            }
+//        },1000);
+
+    }
+}
